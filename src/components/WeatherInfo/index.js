2f6f1d18059d036b16ee0dad0be1360c061813ab
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-10 20:04:21 -0300

    Adding form

--- src/components/WeatherInfo/index.js
+++ src/components/WeatherInfo/index.js
@@ -5,6 +5,10 @@ export default class WeatherInfo extends Component {
     render() {
         return (
             <main className='weather-container'>
+                <form className='form'>
+                    <input className='search' />
+                    <button className='btn-search'>search</button>
+                </form>
                 <h2>Weather in City</h2>
                 <h2>26°C</h2>
             </main>
