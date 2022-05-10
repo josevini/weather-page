280252208d465a95b9bd539ed1a2808f7617dab3
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-10 20:46:40 -0300

    Changing class components to function components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import { Component } from 'react'
 import WeatherInfo from './components/WeatherInfo'
 import './App.css'
 
-export default class App extends Component {
-  render() {
-    return (
-      <main className='main'>
-        <WeatherInfo />
-      </main>
-    )
-  }
+export default function App () {
+  return (
+    <main className='main'>
+      <WeatherInfo />
+    </main>
+  )
 }
--- src/components/WeatherInfo/index.js
+++ src/components/WeatherInfo/index.js
@@ -1,20 +1,17 @@
-import { Component } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import './styles.css'
 
-export default class WeatherInfo extends Component {
-    render() {
-        return (
-            <main className='weather-container'>
-                <form className='form'>
-                    <input className='search' placeholder='Search' />
-                    <button className='btn-search'>
-                        <FaSearch />
-                    </button>
-                </form>
-                <h2>Weather in City</h2>
-                <h2>26°C</h2>
-            </main>
-        )
-    }
+export default function WeatherInfo () {
+    return (
+        <main className='weather-container'>
+            <form className='form'>
+                <input className='search' placeholder='Search' />
+                <button className='btn-search'>
+                    <FaSearch />
+                </button>
+            </form>
+            <h2>Weather in City</h2>
+            <h2>26°C</h2>
+        </main>
+    )
 }
