17123dd992de71c0735e992aecef6cf5d6779f79
Vinicius Silva <ferreiravini468@gmail.com>
2022-05-11 00:59:16 -0300

    Pulling data from API

--- src/components/WeatherInfo/index.js
+++ src/components/WeatherInfo/index.js
@@ -1,17 +1,37 @@
+import { useState } from 'react'
 import { FaSearch } from 'react-icons/fa'
+import request from '../../api/api'
 import './styles.css'
 
 export default function WeatherInfo () {
+    const [valueSearch, setValueSearch] = useState('')
+    const [city, setCity] = useState('?')
+    const [temp, setTemp] = useState(0)
+
+
     return (
         <main className='weather-container'>
-            <form className='form'>
-                <input className='search' placeholder='Search' />
-                <button className='btn-search'>
+            <div className='form'>
+                <input className='search' placeholder='Search'
+                    onChange={(event) => {
+                        setValueSearch(event.target.value)
+                    }}
+                />
+                <button className='btn-search'
+                    onClick={() => {
+                        request(valueSearch, 'metric')
+                        .then(response => response.json())
+                        .then(json => {
+                            setCity(json.name)
+                            setTemp(json.main.temp)
+                        })
+                    }}
+                >
                     <FaSearch />
                 </button>
-            </form>
-            <h2>Weather in City</h2>
-            <h2>26°C</h2>
+            </div>
+            <h2>Weather in {city}</h2>
+            <h2>{temp}°C</h2>
         </main>
     )
 }
