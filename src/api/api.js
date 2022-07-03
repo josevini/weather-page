41578c4442860fba5013640f115fcd45dd27afc4
Vinicius Silva <ferreiravini468@gmail.com>
2022-07-03 11:57:49 -0300

    api.js removed

--- src/api/api.js
+++
@@ -1,3 +0,0 @@
-export default async function request(city, units) {
-    return await fetch(`${process.env.REACT_APP_URL}?appid=${process.env.REACT_APP_KEY}&q=${encodeURI(city)}&units=${units}`)
-}
