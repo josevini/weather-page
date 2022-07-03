967de4119a57bfc19e0699653431641c057f6bef
Vinicius Silva <ferreiravini468@gmail.com>
2022-07-03 11:53:12 -0300

    Creating App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import WeatherInfo from './components/WeatherInfo'
+import './App.css'
+
+export default function App() {
+	return (
+		<main className='main'>
+			<WeatherInfo />
+		</main>
+	)
+}
