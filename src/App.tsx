23fa02bc5b6fbc23119c89959d717744f23cf3ba
Vinicius Silva <ferreiravini468@gmail.com>
2022-07-04 21:09:36 -0300

    Updating App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import WeatherInfo from './components/WeatherInfo'
-import './App.css'
+import day_city from './assets/day-city.jpg'
+
+const Main = styled.main`
+	width: 100vw;
+	height: 100vh;
+	background-image: url(${day_city});
+	background-size: cover;
+	background-position: center center;
+`
 
 export default function App() {
 	return (
-		<main className='main'>
+		<Main>
 			<WeatherInfo />
-		</main>
+		</Main>
 	)
 }
