8713733fe5e12ce74202a04d38163e32c12a3ffc
Vinicius Silva <ferreiravini468@gmail.com>
2022-07-04 21:09:23 -0300

    Updating api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
-
+import { WeatherResponse } from '../interfaces'
 
 export async function request(city: string, units: string) {
-	return await axios.get(`${process.env.REACT_APP_URL}?appid=${process.env.REACT_APP_KEY}&q=${encodeURI(city)}&units=${units}`).then(response => {
+	return await axios.get<WeatherResponse>(`${process.env.REACT_APP_URL}?appid=${process.env.REACT_APP_KEY}&q=${encodeURI(city)}&units=${units}`).then(response => {
 		return response
 	})
 }
